567a425b2c8d1d4db935662b12f96fb86808635f
David Glymph <Woozl@users.noreply.github.com>
2023-03-29 11:41:47 -0400

    fetch graph source metadata

--- src/theme/DocCard/index.js
+++ src/theme/DocCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import {
@@ -35,10 +35,68 @@ function CardLayout({ href, icon, title, description }) {
     </CardContainer>
   );
 }
+
+const SOURCES = new Set([
+  "biolink",
+  "ctd",
+  "drugcentral",
+  "gtex",
+  "gtopdb",
+  "gwas-catalog",
+  "hetio",
+  "hgnc",
+  "hmdb",
+  "human-goa",
+  "icees-kg",
+  "intact",
+  "ontological-hierarchy",
+  "panther",
+  "pharos",
+  "robokopkg",
+  "string-db",
+  "uberongraph",
+  "viral-proteome",
+  "automat",
+]);
+
+const getMetaData = async (label) => {
+  if (!SOURCES.has(label)) {
+    return null;
+  }
+
+  try {
+    const response = await fetch(
+      `https://automat.renci.org/${label}/metadata`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const { final_node_count, final_edge_count } = await response.json();
+
+    return {
+      final_node_count: parseInt(final_node_count).toLocaleString(),
+      final_edge_count: parseInt(final_edge_count).toLocaleString(),
+    };
+  } catch {
+    return null;
+  }
+};
+
 function CardCategory({ item }) {
   const href = findFirstCategoryLink(item);
   // Unexpected: categories that don't have a link have been filtered upfront
-  console.log(item);
+
+  const [md, setMd] = useState(null);
+  useEffect(() => {
+    (async () => {
+      setMd(await getMetaData(item.label));
+    })();
+  }, []);
+
   if (!href) {
     return null;
   }
@@ -49,12 +107,21 @@ function CardCategory({ item }) {
       title={item.label}
       description={translate(
         {
-          message: "{count} items",
+          message:
+            !md ||
+            md.final_edge_count === "NaN" ||
+            md.final_node_count === "NaN"
+              ? "{count} items"
+              : "{final_node_count} nodes, {final_edge_count} edges",
           id: "theme.docs.DocCard.categoryDescription",
           description:
             "The default description for a category card in the generated index about how many items this category includes",
         },
-        { count: item.items.length }
+        {
+          count: item.items.length,
+          final_node_count: md?.final_node_count,
+          final_edge_count: md?.final_edge_count,
+        }
       )}
     />
   );
