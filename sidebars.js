aa4c5476a1c8a539fb4d877c65454daf8bb15358
David Glymph <Woozl@users.noreply.github.com>
2023-05-30 22:17:56 -0400

    add introduction page

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   openApiSidebar: [
+    "intro/intro",
     {
       type: "category",
       label: "Automat",
